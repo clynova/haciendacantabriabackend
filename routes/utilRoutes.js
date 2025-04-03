e72b4285828bbd425820157aecb5acd06ed6fdc8
Sergio Basaez <50474233+Wixtux@users.noreply.github.com>
2025-04-02 21:36:41 -0300

    Trop products
     se creo un nuevo controler que te da los productos mas vendidos

--- routes/utilRoutes.js
+++ routes/utilRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getDashboardStats, getTopTags, getTotalSales, getQuotationStats, getOrderStats } from '../controllers/utilController.js';
+import { getDashboardStats, getTopTags, getTotalSales, getQuotationStats, getOrderStats, getTopProducts } from '../controllers/utilController.js';
 import { checkAuth } from '../middleware/authMiddleware.js';
 import { handleContactForm } from '../controllers/contactController.js';
 import { enviarEmailConfirmacionOrden } from '../controllers/emailController.js';
@@ -11,7 +11,7 @@ utilRoutes.get('/top-tags', checkAuth, getTopTags);
 utilRoutes.get('/total-sales', checkAuth, getTotalSales);
 utilRoutes.get('/quotation-stats', checkAuth, getQuotationStats);
 utilRoutes.get('/order-stats', checkAuth, getOrderStats);
-
+utilRoutes.get('/top-products', getTopProducts); // Nueva ruta
 
 utilRoutes.post('/contact', handleContactForm);
 utilRoutes.get('/send-emailOrder/:orderId', checkAuth, enviarEmailConfirmacionOrden);
